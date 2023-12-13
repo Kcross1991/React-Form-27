09e164a1c1c95bb6fc45767e1ba0e9766151f938
root <root@Kacy>
2023-12-13 15:03:58 -0600

    token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import './App.css'
-
+import SignUpForm from './SignUpForm'
+import Authenticate from './Authenticate'
+import { useState } from 'react';
     
-     
+  export default function App(){
+    const [token, setToken] = useState(null);
+   
+    return (
+      <>
+      <Authenticate token={token} setToken={setToken}/>
+      <SignUpForm token={token} setToken={setToken} />
+      </>
+    );
+  }  
 
 
