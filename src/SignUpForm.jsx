ab79193a2f1f34630559faba9b9938807256a0fa
root <root@Kacy>
2023-12-14 08:40:13 -0600

    Got most of it done

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function SignUpForm({setToken}){
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
     const [error,setError] = useState(null);
+    const [errors, setErrors] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && submitting) {
+            signUp();
+        }
+    }, [errors]);
+
+    const validateForm =() => {
+        const errors =[]
+
+        if (username.length >6){
+            setError("Username must be 6 characters");
+            
+       }
+         if (password.length >12){
+               setError("Password must be 12 characters");
+               
+           }
+           return errors
+        
+       
+
+    }
 
     async function handleSubmit(event){
         event.preventDefault();
+        setErrors(validateForm)
+        setSubmitting(validateForm)
         console.log("Hello ");
 
+       
+        
         try{
             const response =await fetch(
                 "https://fsa-jwt-practice.herokuapp.com/signup",
@@ -23,24 +53,30 @@ export default function SignUpForm({setToken}){
                 })
                 const result = await response.json();
                 setToken(result.token)
+                setUsername("")
+                setPassword("")
                 console.log(result)
+                
         
         }catch (error){
         setError(error.message);
         }
     }
-  
+    
+
     return (
         <>
         <h2>Sign Up</h2>
-        {error &&<p>{error}</p>}
+
     <form onSubmit={handleSubmit}> 
         <label>Username: 
       <input 
       value={username}
       onChange={(e) => 
         setUsername(e.target.value)
-    } />
+    } 
+    required
+    />
      </label>
 
      <label>
@@ -51,11 +87,19 @@ export default function SignUpForm({setToken}){
       onChange={(e) => 
         setPassword(e.target.value)
     }
+    required
      />
      </label>
 
      <button>Submit</button>
     </form>
+
+    {Object.keys(errors).length === 0 && submitting ? (
+                    <span className="success">Successfully submitted </span>
+                ) : null}
+
+                {errors.username ? <p className="error">{errors.username}</p> : null}
+                {errors.password ? <p className="error">{errors.password}</p> : null}
     </>
     );
 }
